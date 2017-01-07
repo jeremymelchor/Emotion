15029bd776b1bb3ffcb5d6a9dbd06d1bbe4f5963
aqmebrak <aqmebrak@gmail.com>
2017-01-07 19:19:48 +0100

    port config

--- server/index.js
+++ server/index.js
@@ -41,7 +41,8 @@ io.on('connection', function(socket){
 		io.emit('broadcastToAll_chatMessage', sockectObj);
 	});
 });
+var port = process.env.PORT || 3000;
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(port, function(){
+  console.log('listening on *:'+port);
 });
